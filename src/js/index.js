adb948c55fc23156dfca727d6a631aebc8bde8a0
dimasgit <aansaputra8890@gmail.com>
2021-11-17 10:28:57 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import main from './views/main';
+import "../css/style.css";
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { resolve } = require("path");
+
+module.exports = {
+    input: "src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader"
+            ]
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.min.css"
+        })
+    ]
+}
