7cddd646b3969e5929b01fcd58f809787f4f3c1f
dimasgit <aansaputra8890@gmail.com>
2021-11-17 14:49:11 +0700

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import main from './views/main';
 import "../css/style.css";
+import "animate.css";
 
 main();
--- src/js/views/main.js
+++ src/js/views/main.js
@@ -1,6 +1,37 @@
+import Particles from "particlesjs/dist/particles.min.js";
 const main = () => {
+Particles.init({
+// normal options
+    selector: '.background',
+    maxParticles: 450,
 
-};
+// options for breakpoints
+    responsive: [{
+        breakpoint: 1367,
+        options: {
+        maxParticles: 150,
+        color: '#FF0000',
+        connectParticles: true
+    }
+    },
+    {
+        breakpoint: 786,
+        options: {
+        maxParticles: 100,
+        connectParticles: true
+        }
+    }, 
+    {
+        breakpoint: 320,
+        options: {
+        maxParticles: 0
+// disables particles.js
+        }
+    }
+]
+});
+
+}
 
 
 export default main;
