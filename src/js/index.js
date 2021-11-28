e2ba1ef7405771b12796251ad67699b3d78b093c
andrianfaa <andrianfadhilla@gmail.com>
2021-11-28 11:57:37 +0700

    added sample api requests and added simple state management

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
-import main from './views/main';
+import "regenerator-runtime";
+import main from "./views/main";
+import GithubApi from "./services/GithubApi";
+import StateManagement from "./services/StateManagement";
 // import "animate.css";
 import "../css/style.css";
-import "../css/responsive.css"
+import "../css/responsive.css";
 
 main();
+
+const state = new StateManagement();
+
+async function getData() {
+    const Api = new GithubApi("infinitedim");
+    const data = await Api.getData();
+
+    state.setState({ githubData: data });
+}
+
+getData();
