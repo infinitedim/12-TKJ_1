7f5e836c31432ba1b3a8cd4583d1ff57855f2449
andrianfaa <andrianfadhilla@gmail.com>
2021-11-28 11:58:32 +0700

    remove some useless code

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { resolve } = require("path");
 
 module.exports = {
     entry: "./src/js/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
     },
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader'
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                      plugins: ['autoprefixer']
-                  }
-              }
-            }
-            ]
-          },
-          {
-            test: /\.js$/i,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }]
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+            },
+            {
+                test: /\.js$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
-            filename: "index.html"
+            filename: "index.html",
+            inject: "body",
+            hash: true,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+                minifyURLs: true,
+            },
         }),
         new MiniCssExtractPlugin({
-            filename: "style.min.css"
-        })
-    ]
-}
+            filename: "style.min.css",
+        }),
+    ],
+};
