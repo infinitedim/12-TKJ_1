9782fe508d8721dd73471b347515f6fc347b8af1
dimasgit <aansaputra8890@gmail.com>
2022-01-15 13:47:59 +0700

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import "regenerator-runtime";
 import main from "./views/main";
 import StudentApi from "./services/StudentApi";
-// import "animate.css";
 import "../css/style.css";
 import "../css/responsive.css";
 
@@ -10,7 +9,19 @@ main();
 async function fetchData() {
     const data = await StudentApi.getStudents();
 
-    console.log(await data);
+    // console.log(await data);
+    const showCards = await (data) => {
+        return /*html*/ `<div class="col-md-4 my-5 text-center">
+                            <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="${data.photo_picture}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h1 class="card-text">${data.name}</h1>
+                                    <p class="card-text">${data.future_goals}</p>
+                                </div>
+                            </div>
+                        </div>`;
+    };
+
 }
 
 fetchData();
