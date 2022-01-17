f5397525bff7a19079adfc10271fe8b6883af545
dimasgit <aansaputra8890@gmail.com>
2022-01-17 10:10:55 +0700

    n

--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,7 @@ main();
 async function fetchData() {
     const data = await StudentApi.getStudents();
 
-    // console.log(await data);
-    const showCards = await (data) => {
-        return /*html*/ `<div class="col-md-4 my-5 text-center">
-                            <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="${data.photo_picture}" alt="Card image cap">
-                                <div class="card-body">
-                                    <h1 class="card-text">${data.name}</h1>
-                                    <p class="card-text">${data.future_goals}</p>
-                                </div>
-                            </div>
-                        </div>`;
-    };
-
+    console.log(await data);
 }
 
 fetchData();
