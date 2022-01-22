13f0d89829e8e3304ac39648acf05627c91ed11e
Dimas Saputra <69130180+infinitedim@users.noreply.github.com>
2022-01-22 10:49:34 +0700

    Add files via upload
    Fixing card size

--- src/js/views/cards.js
+++ src/js/views/cards.js
@@ -1,6 +1,6 @@
 const cards = (student) => {
     return /*html*/ `<div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card">
                             <img class="card-img-top" src="${student.photo_picture}" alt="Card image cap">
                             <div class="card-body">
                             <h1>${student.name}</h1>
